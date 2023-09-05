b27003e99242bab3db9f7d8d99ff60862a1b179b
Kelvin <kvalera200244@gmail.com>
2023-09-04 22:05:21 -0400

    up

--- src/components/form/fom.tsx
+++ src/components/form/fom.tsx
@@ -89,7 +89,7 @@ function Form() {
             textTransform="uppercase"
             className={Style.formTitle}
           >
-            Todo lo hacemos posibleee, sólo contáctanos y cuéntanos sobre tu
+            Todo lo hacemos posibleeee, sólo contáctanos y cuéntanos sobre tu
             proyecto.
           </Heading>
         </CardHeader>

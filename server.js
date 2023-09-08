47742545b73b8d84dd670e80188e5bf9682be765
Kelvin <kvalera200244@gmail.com>
2023-09-07 23:08:40 -0400

    up 4

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const nodemailer = require("nodemailer");
 const cors = require("cors");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use((req, res, next) => {
@@ -11,7 +12,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/send-email", (req, res) => {
   const { name, email, phone, comment, service } = req.body;
